0a6e7d18730252adb99b7353bf18bee8bb4aeda8
jason <markjasongalang3@gmail.com>
2026-09-22 16:59:01 +0800

    feat: add delta/accumulator method (game loop approach 2)

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -42,39 +42,78 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start(); // Calls the run() below
     }
 
+//    /**
+//     * Sleep method
+//     */
+//    @Override
+//    public void run() {
+//        double drawInterval = 1_000_000_000 / FPS; // 0.0166667 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval; // The allocated time for a single loop is 0.0166667 seconds
+//
+//        while (gameThread != null) {
+//            System.out.println("The game loop is running");
+//
+//            long currentTime = System.nanoTime(); // Returns the current value of the running JVM's high-res time source in nanoseconds
+//            System.out.println("Current time: " + currentTime);
+//
+//            // 1. UPDATE: update information such as character position
+//            update();
+//
+//            // 2. DRAW: draw the screen with the updated information
+//            repaint(); // Calls the paintComponent(Graphics g) below
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime /= 1_000_000; // Convert nanoseconds to milliseconds
+//
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
     /**
-     * Sleep method
+     * Delta/Accumulator method
      */
     @Override
     public void run() {
-        double drawInterval = 1_000_000_000 / FPS; // 0.0166667 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval; // The allocated time for a single loop is 0.0166667 seconds
+        double drawInterval = 1_000_000_000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
         while (gameThread != null) {
-//            System.out.println("The game loop is running");
+            currentTime = System.nanoTime();
 
-//            long currentTime = System.nanoTime(); // Returns the current value of the running JVM's high-res time source in nanoseconds
-//            System.out.println("Current time: " + currentTime);
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-            // 1. UPDATE: update information such as character position
-            update();
+            // When 'delta' reaches draw interval, we update the screen
+            // e.g., it becomes 0.0166667 (or more) by adding the elapsed time (currentTime - lastTime)
+            // and we try to divide it by drawInterval, then surely the quotient will become greater than 1
+            // (Basically, this will be true once the numerator > denominator)
+            if (delta >= 1) {
+                update();
+                repaint();
 
-            // 2. DRAW: draw the screen with the updated information
-            repaint(); // Calls the paintComponent(Graphics g) below
+                delta--;
+                drawCount++;
+            }
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime /= 1_000_000; // Convert nanoseconds to milliseconds
-
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if (timer >= 1_000_000_000) { // 1 second
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
             }
         }
     }
